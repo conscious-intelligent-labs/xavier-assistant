e356c173e950eb739a09cee7255a4d02aacc7c31
jay <wright.jonmarc@gmail.com>
2019-08-05 07:32:38 -0500

    added code to listen for a command he recognizes in a loop and respond

--- xavier/poc/xavier-start.py
+++ xavier/poc/xavier-start.py
@@ -5,26 +5,43 @@ import os
 speech_engine = pyttsx3.init()
 speech_engine.setProperty('rate', 150)
 
-def speak(text):
+# Define what to say
+def xavierResponse(audio) :
+    print(audio)
+    for line in audio.splitlines() :
+        xavierSays(audio)
+# Turn on the mouth
+def xavierSays(text):
 	speech_engine.say(text)
 	speech_engine.runAndWait()
 
 recognizer = speech_recognition.Recognizer()
 
-def listen():
+# Turn on the ears
+def xavierHears():
     with speech_recognition.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source)
+        recognizer.adjust_for_ambient_noise(source, duration = 1)
         audio = recognizer.listen(source)
-
     try:
-        return recognizer.recognize_google(audio)
-# or: return recognizer.recognize_google(audio)
+        request = recognizer.recognize_google(audio).lower()
+        # Having Xavier repeat what he thinks he heard for debugging
+        xavierResponse("You said: " + request + "\n")
+        return request
     except speech_recognition.UnknownValueError:
-        speak("I could not understand what you said")
-        return False
+        print("I didn't catch that...")
+        request = xavierHears()
     except speech_recognition.RequestError as e:
         print("Recog Error; {0}".format(e))
-    return ""
+    return request
+
+# Turn on the brain
+def xavierThinks(request) :
+    if "hello xavier" in request :
+        xavierResponse("Hello there")
+
+    elif "go to sleep" in request :
+        xavierResponse("okay, heading to bed!")
+        exit(0)
+
 while True :
-    print("Say something...")
-    speak("I heard you say " + listen())
+    xavierThinks(xavierHears())
