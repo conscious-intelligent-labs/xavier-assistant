0cf0d1d3a905452e3258662d792b51bfba1b0822
jay <wright.jonmarc@gmail.com>
2019-08-04 22:39:48 -0500

    uploading some test cases for proof of concept stuff

---
+++ xavier/poc/xavier-start.py
@@ -0,0 +1,29 @@
+import speech_recognition
+import pyttsx3
+import os
+
+speech_engine = pyttsx3.init()
+speech_engine.setProperty('rate', 150)
+
+def speak(text):
+	speech_engine.say(text)
+	speech_engine.runAndWait()
+
+recognizer = speech_recognition.Recognizer()
+
+def listen():
+    with speech_recognition.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        return recognizer.recognize_google(audio)
+# or: return recognizer.recognize_google(audio)
+    except speech_recognition.UnknownValueError:
+        print("Could not understand audio")
+    except speech_recognition.RequestError as e:
+        print("Recog Error; {0}".format(e))
+    return ""
+while True :
+    print("Say something...")
+    speak("I heard you say " + listen())
