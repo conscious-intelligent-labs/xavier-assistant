546cc9ede7a5a32408ae04fc3aa94510286d123b
System Administrator <root@Drs-MacBook-Pro.local>
2019-08-13 21:32:50 -0500

    Add setup files

---
+++ xavier/xavier/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##Uncomment if you want to start virtual environment.
+#source bin/activate
+
+# Start server.
+sudo python ../manage.py runserver&
+
---
+++ xavier/xavier/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+python3 -m pip install -r requirements.txt
+echo "Done installing dependencies"
+
+echo "Set Python Path"
+
+echo "Python path set."
+
+FILE=bin/activate
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+    source bin/activate
+else 
+    echo "$FILE does not exist"
+    echo "Creating Virtual Environment"
+    python3 -m venv ./
+fi
+
+echo "Done setup!"
