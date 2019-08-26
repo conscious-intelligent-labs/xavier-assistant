e1feec8cee697530d04397c2cd4972b2dba8458c
Kyle Morton <kfmorton@us.ibm.com@Kyles-MBP.softlayer.local>
2019-08-26 16:56:55 -0500

    added mimic to setup

--- xavier/xavier/setup.sh
+++ xavier/xavier/setup.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-echo "Installing dependencies..."
+echo "Installing xavier dependencies..."
 python3 -m pip install -r requirements.txt
+
+echo "Installing Mimic dependencies..."
+apt-get install gcc make pkg-config automake libtool libasound2-dev
+
 echo "Done installing dependencies"
 
-echo "Set Python Path"
+echo "Installing Mimic..."
+cd /opt/
+git clone https://github.com/MycroftAI/mimic1.git mimic
+cd /opt/mimic
+./dependencies.sh --prefix="/usr/local"
+./autogen.sh
+./configure --prefix="/usr/local"
+make
+make check
 
-echo "Python path set."
+echo "Setting mimic path"
+
+export PATH=$PATH:/opt/mimic
+
+echo "Mimic install complete"
+
+echo "Testing Mimic Install..."
+./mimic -t "Mimic is working."
+
 
 FILE=bin/activate
 if [ -f "$FILE" ]; then
