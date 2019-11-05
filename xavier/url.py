a98f422ad81dde264341845ecf2eded081ec17d2
Dr. Kyle Morton <kfmorton@Drs-MacBook-Pro.local>
2019-11-04 22:34:05 -0600

    update

--- xavier/url.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('skills/', include('skills.urls')),
-    path('admin/', admin.site.urls),
-]
--- xavier/xavier/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-##Uncomment if you want to start virtual environment.
-source bin/activate
-
-# Start server.
-sudo python3 ../manage.py runserver&
